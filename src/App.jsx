1f822d49ac09942d1e54a8f7ff928aa21d2f9f8f
Eligio Elizarraraz <eligio2008@live.com.mx>
2024-11-23 13:22:19 -0600

    chore: added HTML basic structure and meta tags from old project

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,69 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import logo from "./assets/images/logo.svg";
+import profileImage from "./assets/images/profile1.jpg";
+import editIcon from "./assets/images/edit_icon.svg";
+import plusButton from "./assets/images/boton_mas.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class="page">
+        <header class="header">
+          <img
+            src={logo}
+            alt="Logo Alrededor de los Estados Unidos"
+            class="header__logo"
+          />
+          <div class="header__line"></div>
+        </header>
+
+        <main class="content">
+          <section class="profile">
+            <div class="profile__image">
+              <img
+                src={profileImage}
+                alt="Foto de perfil del usuario"
+                class="profile__image-user"
+              />
+              <img
+                src={editIcon}
+                alt="Editar perfil"
+                class="profile__image-edit"
+              />
+            </div>
+
+            <div class="profile__info">
+              <div class="profile__heading">
+                <h1 class="profile__name">Jacques Costeau</h1>
+                <div class="profile__edit">
+                  <img
+                    src={editIcon}
+                    alt="Boton editar"
+                    class="profile__edit-image"
+                  />
+                </div>
+              </div>
+              <h2 class="profile__hobby">Explorador</h2>
+            </div>
+
+            <div class="profile__add">
+              <img
+                src={plusButton}
+                alt="Botón más"
+                class="profile__add-image"
+              />
+            </div>
+          </section>
+
+          <section class="photos"></section>
+        </main>
+
+        <footer class="footer">
+          <p class="footer__copyright">
+            &#169; 2024 Around the U.S. by Eligio Elizarraraz
+          </p>
+        </footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
