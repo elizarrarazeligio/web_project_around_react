46314417209321f3ca4c3b31ea7f03f01c91733a
Eligio Elizarraraz <eligio2008@live.com.mx>
2024-11-24 18:13:30 -0600

    feat: added functionality to editProfile, editAvatar and newCard popups through state hook

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,43 @@
 import profileImage from "../../assets/images/profile1.jpg";
 import editIcon from "../../assets/images/edit_icon.svg";
 import plusButton from "../../assets/images/boton_mas.svg";
+import { useState } from "react";
+import Popup from "./Popup/Popup";
+import NewCard from "./Popup/form/NewCard/NewCard";
+import EditProfile from "./Popup/form/EditProfile/EditProfile";
+import EditAvatar from "./Popup/form/EditAvatar/EditAvatar";
 
 function Main() {
+  const [popup, setPopup] = useState(null);
+
+  // Asignación de props a variables para generación de Popups
+  const newCardPopup = { title: "Nuevo lugar", children: <NewCard /> };
+  const EditProfilePopup = {
+    title: "Editar perfil",
+    children: <EditProfile />,
+  };
+  const editAvatarPopup = {
+    title: "Cambiar foto de perfil",
+    children: <EditAvatar />,
+  };
+
+  // Función para abrir Popup
+  function handleOpenPopup(popup) {
+    setPopup(popup);
+  }
+
+  // Función para cerrar Popup
+  function handleClosePopup() {
+    setPopup(null);
+  }
+
   return (
     <main className="content">
       <section className="profile">
-        <div className="profile__image">
+        <div
+          className="profile__image"
+          onClick={() => handleOpenPopup(editAvatarPopup)}
+        >
           <img
             src={profileImage}
             alt="Foto de perfil del usuario"
@@ -22,7 +53,10 @@ function Main() {
         <div className="profile__info">
           <div className="profile__heading">
             <h1 className="profile__name">Jacques Costeau</h1>
-            <div className="profile__edit">
+            <div
+              className="profile__edit"
+              onClick={() => handleOpenPopup(EditProfilePopup)}
+            >
               <img
                 src={editIcon}
                 alt="Boton editar"
@@ -33,7 +67,10 @@ function Main() {
           <h2 className="profile__hobby">Explorador</h2>
         </div>
 
-        <div className="profile__add">
+        <div
+          className="profile__add"
+          onClick={() => handleOpenPopup(newCardPopup)}
+        >
           <img
             src={plusButton}
             alt="Botón más"
@@ -43,6 +80,13 @@ function Main() {
       </section>
 
       <section className="photos"></section>
+
+      {/* Evaluando hook para abrir Popups */}
+      {popup && (
+        <Popup onClose={handleClosePopup} title={popup.title}>
+          {popup.children}
+        </Popup>
+      )}
     </main>
   );
 }
--- src/components/Main/Popup/Popup.jsx
+++ src/components/Main/Popup/Popup.jsx
@@ -1,13 +1,16 @@
+import closeButton from "../../../assets/images/close_icon.svg";
+
 export default function Popup(props) {
-  const { title, children } = props;
+  const { onClose, title, children } = props;
 
   return (
     <section className="popup">
       <div className="popup__container">
         <img
-          src="./images/close_icon.svg"
+          src={closeButton}
           alt="Botón cerrar formulario"
           className="popup__close"
+          onClick={onClose}
         />
         <h2 className="popup__title">{title}</h2>
         {children}
