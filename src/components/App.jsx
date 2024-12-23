094144c3b53627580f5b3ff096bdf4ff1ec519ca
Eligio Elizarraraz <eligio2008@live.com.mx>
2024-12-23 11:43:16 -0600

    feat: created currentUser state in App to retrieve user information thorugh api request

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,16 @@ import Header from "./Header/Header";
 import Main from "./Main/Main";
 import Footer from "./Footer/Footer";
 import "../index.css";
+import { useState, useEffect } from "react";
+import { api } from "../utils/api";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState({});
+
+  useEffect(() => {
+    api.getUserInfo().then((data) => setCurrentUser(data));
+  }, []);
+
   return (
     <>
       <div className="page">
